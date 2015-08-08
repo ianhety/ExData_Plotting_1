7d717bf7c64897794258022fa9a7feaf027d71b2
ianhety <geneivan@hotmail.com>
2015-08-08 14:04:51 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,9 +1,8 @@
 # plot1.R for Global Active Power
 
-# set directory
-setwd("C:/Coursera/Dataset")
+# please set your directory first
 
-# Get data from 1/2/20017 to 2/2/2007
+# Get data from 1/2/20017(row66638) to 2/2/2007(row69517)
 data1 <- read.csv("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")[66638:69517,]
 
 # Plot the hist and save
